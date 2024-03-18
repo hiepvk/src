f635ad73194611408a475bd9f4a6037239f88b50
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2024-03-18 23:29:19 +0700

    Update Hiepvk.h

--- Sources/Hiepvk.h
+++ Sources/Hiepvk.h
@@ -42,6 +42,49 @@
 #define YT_BUNDLE_ID @"com.google.ios.youtube"
 #define YT_NAME @"YouTube"
 
+#define DEFAULT_RATE 1.0f
+
+
+@interface YTVarispeedSwitchControllerOption : NSObject
+- (id)initWithTitle:(id)title rate:(float)rate;
+@end
+
+
+@interface YTPlayerViewController : NSObject
+@property id activeVideo;
+@property float playbackRate;
+- (void)singleVideo:(id)video playbackRateDidChange:(float)rate;
+@end
+
+
+@interface MLHAMQueuePlayer : NSObject
+@property id playerEventCenter;
+@property id delegate;
+- (void)setRate:(float)rate;
+- (void)internalSetRate;
+@end
+
+
+@interface MLPlayerStickySettings : NSObject
+- (void)setRate:(float)rate;
+@end
+
+
+@interface MLPlayerEventCenter : NSObject
+- (void)broadcastRateChange:(float)rate;
+@end
+
+
+@interface YTSingleVideoController : NSObject
+- (void)playerRateDidChange:(float)rate;
+@end
+
+
+@interface HAMPlayerInternal : NSObject
+- (void)setRate:(float)rate;
+@end
+
+
 // IAmYouTube
 @interface SSOConfiguration : NSObject
 @end
