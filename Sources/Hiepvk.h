d03963cdc44aa862125392f6383701027fdef471
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2024-03-18 18:49:46 +0700

    Update Hiepvk.h

--- Sources/Hiepvk.h
+++ Sources/Hiepvk.h
@@ -49,13 +49,6 @@
 @end
 
 
-@interface YTPlayerViewController : NSObject
-@property id activeVideo;
-@property float playbackRate;
-- (void)singleVideo:(id)video playbackRateDidChange:(float)rate;
-@end
-
-
 @interface MLHAMQueuePlayer : NSObject
 @property id playerEventCenter;
 @property id delegate;
